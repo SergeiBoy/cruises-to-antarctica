004164ab982c9e3d957d5d9c6761e9c60effd68a
SergeiBoy <s_shchetinin@mail.ru>
2020-10-19 02:15:40 +0500

    Резервное копирование

---
+++ source/js/menu.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  var header = document.querySelector('.page-header');
+  var navToggle = document.querySelector('.page-header__toggle');
+
+  header.classList.add('page-header--menu-closed');
+
+  navToggle.addEventListener('click', function() {
+    if (header.classList.contains('page-header--menu-closed')) {
+      header.classList.remove('page-header--menu-closed');
+      header.classList.add('page-header--menu-opened');
+    } else {
+      header.classList.add('page-header--menu-closed');
+      header.classList.remove('page-header--menu-opened');
+    }
+  });
+})();
