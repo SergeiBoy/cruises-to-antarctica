c9b670c8ee9cf5518735613626e14b58a56da240
SergeiBoy <s_shchetinin@mail.ru>
2020-10-19 11:41:22 +0500

    Сделан header и footer

--- build/js/menu.js
+++ build/js/menu.js
@@ -6,7 +6,7 @@
 
   header.classList.add('page-header--menu-closed');
 
-  navToggle.addEventListener('click', function() {
+  navToggle.addEventListener('click', function () {
     if (header.classList.contains('page-header--menu-closed')) {
       header.classList.remove('page-header--menu-closed');
       header.classList.add('page-header--menu-opened');
